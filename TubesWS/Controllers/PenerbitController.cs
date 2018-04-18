37a03a2eac7b3117c8490d8090c31a63d58d4391
mfarismuzakki <mfarismuzakki@gmail.com>
2018-04-18 10:40:23 +0700

    repo bahasa + bahasa controller

---
+++ TubesWS/Controllers/PenerbitController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TubesWS.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Perbit")]
+    public class PenerbitController : Controller
+    {
+        // GET: api/Perbit
+        [HttpGet]
+        public IEnumerable<Object.Penerbit> Get()
+        {
+            Repository.RepositoryPenerbit penerbit = new Repository.RepositoryPenerbit();
+
+            return penerbit.GetAllPenerbit();
+        }
+
+        // GET: api/Perbit/5
+        [HttpGet("{id}", Name = "Get")]
+        public Object.Penerbit Get(int id)
+        {
+            Repository.RepositoryPenerbit penerbit = new Repository.RepositoryPenerbit();
+
+            return penerbit.GetOnePenerbit(id);
+        }
+        
+        // POST: api/Perbit
+        [HttpPost]
+        public string Post([FromBody]Object.Penerbit value)
+        {
+            try
+            {
+                //deklarasi variavel untuk post
+                Repository.RepositoryPenerbit penerbit = new Repository.RepositoryPenerbit();
+
+                penerbit.InsertPenerbit(value);
+                return "Data Berhasil Diinput";
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+        }
+        
+        // PUT: api/Perbit/5
+        [HttpPut("{id}")]
+        public string Put(int id, [FromBody]Object.Penerbit value)
+        {
+            try
+            {
+                //deklarasi variabel untuk update
+                Repository.RepositoryPenerbit penerbit = new Repository.RepositoryPenerbit();
+                penerbit.UpdatePenerbit(value);
+                return "Update berhasil";
+
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+        }
+        
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public string Delete(int id)
+        {
+            try
+            {
+                //deklarasi variabel untuk delete
+                Repository.RepositoryPenerbit penerbit = new Repository.RepositoryPenerbit();
+
+                //eksekusi delete
+                penerbit.DeletePenerbit(id);
+
+                return "Delete berhasil";
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+        }
+    }
+}
---
+++ TubesWS/Repository/RepositoryBahasa.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using Dapper;
+
+namespace TubesWS.Repository
+{
+    public class RepositoryBahasa
+    {
+
+        //atribut
+        MySqlConnection connection;
+
+        //konstruktor deklarasi hak akses
+        public RepositoryBahasa()
+        {
+            connection = new MySqlConnection("server=localhost;Database=perpustakaan;Uid=root");
+        }
+
+        //membuka koneksi
+        public void OpenConnection()
+        {
+            try
+            {
+                connection.Open();
+            }
+            catch (Exception e)
+            {
+
+            }
+        }
+
+        //menutup koneksi
+        public void CloseConnection()
+        {
+            try
+            {
+                connection.Close();
+            }
+            catch (Exception e)
+            {
+
+            }
+        }
+
+        //memasukan input ke database
+        public void InsertBahasa(Object.Bahasa bahasa)
+        {
+            try
+            {
+                string nama_bahasa = bahasa.Nama_bahasa;
+
+                string query = "insert into bahasa values(null,'" + nama_bahasa + "')";
+                OpenConnection();
+
+                connection.Execute(query);
+
+                CloseConnection();
+            }
+            catch (Exception e)
+            {
+
+            }
+
+        }
+
+        //get All penulis
+        public List<Object.Bahasa> GetAllBahasa()
+        {
+            List<Object.Bahasa> bahasa = new List<Object.Bahasa>();
+
+            try
+            {
+                string query = "select *from bahasa";
+                OpenConnection();
+
+                bahasa = connection.Query<Object.Bahasa>(query).ToList();
+
+                CloseConnection();
+            }
+            catch (Exception e)
+            {
+
+            }
+
+
+            return bahasa;
+        }
+
+        //get one penulis
+        public Object.Bahasa GetOneBahasa(int cari)
+        {
+            Object.Bahasa penulis = new Object.Bahasa();
+
+            try
+            {
+                string query = "select *from bahasa where id_bahasa=" + cari;
+                OpenConnection();
+
+                penulis = connection.Query<Object.Bahasa>(query, new { cari }).FirstOrDefault();
+
+                CloseConnection();
+            }
+            catch (Exception e)
+            {
+
+            }
+
+            return penulis;
+        }
+
+        //update penulis
+        public void UpdateBahasa(Object.Bahasa bahasa)
+        {
+            int id = bahasa.Id_bahasa;
+            string nama_bahasa = bahasa.Nama_bahasa;
+            
+            try
+            {
+                string query = "update bahasa set id_bahasa=" + id + ", nama_bahasa = '" + nama_bahasa + "')";
+                OpenConnection();
+
+                connection.Execute(query);
+
+                CloseConnection();
+            }
+            catch (Exception e)
+            {
+
+            }
+        }
+
+        //delete penulis
+        public void DeleteBahasa(int id)
+        {
+
+            try
+            {
+                string query = "delete from bahasa where id_bahasa= " + id;
+                OpenConnection();
+
+                connection.Execute(query);
+
+                CloseConnection();
+            }
+            catch (Exception e)
+            {
+
+            }
+        }
+    }
+}
