38728289f414e302c8b7cf7ff1376ec9c24f39f8
Demonios41 <36494514+Demonios41@users.noreply.github.com>
2018-05-15 22:25:57 +0700

    GetBy... Objek Buku

--- TubesWS/Repository/RepositoryBuku.cs
+++ TubesWS/Repository/RepositoryBuku.cs
@@ -117,8 +117,128 @@ namespace TubesWS.Repository
                     " (judulbuku.id_penerbit = penerbit.id_penerbit) and" +
                     " (judulbuku.id_bahasa = bahasa.id_bahasa) and" +
                     " (judulbuku.id_kategori = kategori.id_kategori)) and " +
-                    "judulbuku.id_penerbit =" + cari;
+                    "judulbuku.id_buku ='" + cari + "'";
                 return connection.Query<Object.Buku>(query, new { cari }).FirstOrDefault();
+            }
+        }
+
+        //get by Judul Buku
+        public List<Object.Buku> GetByJudulBuku(string cari)
+        {
+            using (connection)
+            {
+                OpenConnection();
+                string query = "select judulbuku.id_buku AS `id_buku`," +
+                    "judulbuku.judul_buku AS `judul`," +
+                    "judulbuku.cetakan AS `cetakan`," +
+                    "judulbuku.tanggalterbit AS `tanggalterbit`," +
+                    "penulis.nama_penulis AS `penulis`," +
+                    "penerbit.nama_penerbit AS `penerbit`," +
+                    "bahasa.nama_bahasa AS `bahasa`," +
+                    "kategori.nama_kategori AS `kategori`" +
+                    " from ((((`judulbuku` join `penulis`) join `penerbit`) join `bahasa`) join `kategori`)" +
+                    " where ((judulbuku.id_penulis = penulis.id_penulis) and" +
+                    " (judulbuku.id_penerbit = penerbit.id_penerbit) and" +
+                    " (judulbuku.id_bahasa = bahasa.id_bahasa) and" +
+                    " (judulbuku.id_kategori = kategori.id_kategori)) and " +
+                    "judulbuku.judul_buku LIKE '%" + cari +"%'";
+                return connection.Query<Object.Buku>(query, new { cari }).ToList();
+            }
+        }
+
+        //get by Penulis Buku
+        public List<Object.Buku> GetByPenulisBuku(string cari)
+        {
+            using (connection)
+            {
+                OpenConnection();
+                string query = "select judulbuku.id_buku AS `id_buku`," +
+                    "judulbuku.judul_buku AS `judul`," +
+                    "judulbuku.cetakan AS `cetakan`," +
+                    "judulbuku.tanggalterbit AS `tanggalterbit`," +
+                    "penulis.nama_penulis AS `penulis`," +
+                    "penerbit.nama_penerbit AS `penerbit`," +
+                    "bahasa.nama_bahasa AS `bahasa`," +
+                    "kategori.nama_kategori AS `kategori`" +
+                    " from ((((`judulbuku` join `penulis`) join `penerbit`) join `bahasa`) join `kategori`)" +
+                    " where ((judulbuku.id_penulis = penulis.id_penulis) and" +
+                    " (judulbuku.id_penerbit = penerbit.id_penerbit) and" +
+                    " (judulbuku.id_bahasa = bahasa.id_bahasa) and" +
+                    " (judulbuku.id_kategori = kategori.id_kategori)) and " +
+                    "penulis.nama_penulis LIKE '%" + cari + "%'";
+                return connection.Query<Object.Buku>(query, new { cari }).ToList();
+            }
+        }
+
+        //get by Penerbit Buku
+        public List<Object.Buku> GetByPenerbitBuku(string cari)
+        {
+            using (connection)
+            {
+                OpenConnection();
+                string query = "select judulbuku.id_buku AS `id_buku`," +
+                    "judulbuku.judul_buku AS `judul`," +
+                    "judulbuku.cetakan AS `cetakan`," +
+                    "judulbuku.tanggalterbit AS `tanggalterbit`," +
+                    "penulis.nama_penulis AS `penulis`," +
+                    "penerbit.nama_penerbit AS `penerbit`," +
+                    "bahasa.nama_bahasa AS `bahasa`," +
+                    "kategori.nama_kategori AS `kategori`" +
+                    " from ((((`judulbuku` join `penulis`) join `penerbit`) join `bahasa`) join `kategori`)" +
+                    " where ((judulbuku.id_penulis = penulis.id_penulis) and" +
+                    " (judulbuku.id_penerbit = penerbit.id_penerbit) and" +
+                    " (judulbuku.id_bahasa = bahasa.id_bahasa) and" +
+                    " (judulbuku.id_kategori = kategori.id_kategori)) and " +
+                    "penerbit.nama_penerbit LIKE '%" + cari + "%'";
+                return connection.Query<Object.Buku>(query, new { cari }).ToList();
+            }
+        }
+
+        //get by Bahasa Buku
+        public List<Object.Buku> GetByBahasaBuku(string cari)
+        {
+            using (connection)
+            {
+                OpenConnection();
+                string query = "select judulbuku.id_buku AS `id_buku`," +
+                    "judulbuku.judul_buku AS `judul`," +
+                    "judulbuku.cetakan AS `cetakan`," +
+                    "judulbuku.tanggalterbit AS `tanggalterbit`," +
+                    "penulis.nama_penulis AS `penulis`," +
+                    "penerbit.nama_penerbit AS `penerbit`," +
+                    "bahasa.nama_bahasa AS `bahasa`," +
+                    "kategori.nama_kategori AS `kategori`" +
+                    " from ((((`judulbuku` join `penulis`) join `penerbit`) join `bahasa`) join `kategori`)" +
+                    " where ((judulbuku.id_penulis = penulis.id_penulis) and" +
+                    " (judulbuku.id_penerbit = penerbit.id_penerbit) and" +
+                    " (judulbuku.id_bahasa = bahasa.id_bahasa) and" +
+                    " (judulbuku.id_kategori = kategori.id_kategori)) and " +
+                    "bahasa.nama_bahasa LIKE '%" + cari + "%'";
+                return connection.Query<Object.Buku>(query, new { cari }).ToList();
+            }
+        }
+
+        //get by Kategori Buku
+        public List<Object.Buku> GetByKategoriBuku(string cari)
+        {
+            using (connection)
+            {
+                OpenConnection();
+                string query = "select judulbuku.id_buku AS `id_buku`," +
+                    "judulbuku.judul_buku AS `judul`," +
+                    "judulbuku.cetakan AS `cetakan`," +
+                    "judulbuku.tanggalterbit AS `tanggalterbit`," +
+                    "penulis.nama_penulis AS `penulis`," +
+                    "penerbit.nama_penerbit AS `penerbit`," +
+                    "bahasa.nama_bahasa AS `bahasa`," +
+                    "kategori.nama_kategori AS `kategori`" +
+                    " from ((((`judulbuku` join `penulis`) join `penerbit`) join `bahasa`) join `kategori`)" +
+                    " where ((judulbuku.id_penulis = penulis.id_penulis) and" +
+                    " (judulbuku.id_penerbit = penerbit.id_penerbit) and" +
+                    " (judulbuku.id_bahasa = bahasa.id_bahasa) and" +
+                    " (judulbuku.id_kategori = kategori.id_kategori)) and " +
+                    "kategori.nama_kategori LIKE '%" + cari + "%'";
+                return connection.Query<Object.Buku>(query, new { cari }).ToList();
             }
         }
 
@@ -150,7 +270,7 @@ namespace TubesWS.Repository
             using (connection)
             {
                 OpenConnection();
-                string query = "update judulbuku set id_buku =" + id_buku + ", judul =" + judul + ", cetakan =" + cetakan + ", tanggalterbit =" + tanggalterbit + ", id_penulis =" + tempPenulis.Id_penulis + ", id_penerbit =" + tempPenerbit.Id_penerbit + ", id_kategori =" + tempKategori.Id_kategori + ", id_bahasa =" + tempBahasa.Id_bahasa + " where id_buku =" + id_buku;
+                string query = "update judulbuku set id_buku ='" + id_buku + "', judul ='" + judul + "', cetakan ='" + cetakan + "', tanggalterbit ='" + tanggalterbit + "', id_penulis ='" + tempPenulis.Id_penulis + "', id_penerbit ='" + tempPenerbit.Id_penerbit + "', id_kategori ='" + tempKategori.Id_kategori + "', id_bahasa ='" + tempBahasa.Id_bahasa + "' where id_buku ='" + id_buku + "'";
                 connection.Execute(query);
             }
 
@@ -162,7 +282,7 @@ namespace TubesWS.Repository
             using (connection)
             {
                 OpenConnection();
-                string query = "delete from judulbuku where id_buku = " + id;
+                string query = "delete from judulbuku where id_buku = '" + id +"'";
                 connection.Execute(query);
             }
         }
