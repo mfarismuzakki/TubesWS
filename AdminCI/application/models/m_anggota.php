6a16a9be7c7ef1e7ae62cfc4e830117b8519494c
basthan <basthan@users.noreply.github.com>
2018-05-30 14:20:36 +0700

    Revert "Revert "AdminCI""
    This reverts commit d1d11077021e9c927d14d1d0b617a0fa2d961e97.

---
+++ AdminCI/application/models/m_anggota.php
@@ -0,0 +1,46 @@
+<?php
+class M_anggota extends CI_Model{
+    private $table="anggotaperpustakaan";
+    private $primary="nama_anggota";
+    private $id="id_anggota";
+    
+    function semua($limit=10,$offset=0,$order_column='',$order_type='asc'){
+        if(empty($order_column) || empty($order_type))
+            $this->db->order_by($this->primary,'asc');
+        else
+            $this->db->order_by($order_column,$order_type);
+        return $this->db->get($this->table,$limit,$offset);
+    }
+    
+    function jumlah(){
+        return $this->db->count_all($this->table);
+    }
+    
+    function cek($kode){
+        $this->db->where($this->id,$kode);
+        $query=$this->db->get($this->table);
+        
+        return $query;
+    }
+    
+    function simpan($jenis){
+        $this->db->insert($this->table,$jenis);
+        return $this->db->insert_id();
+    }
+    
+    function update($kode,$jenis){
+        $this->db->where($this->id,$kode);
+        $this->db->update($this->table,$jenis);
+    }
+    
+    function hapus($kode){
+        $this->db->where($this->id,$kode);
+        $this->db->delete($this->table);
+    }
+    
+    function cari($cari){
+        $this->db->like($this->id,$cari);
+        $this->db->or_like("nama_anggota",$cari);
+        return $this->db->get($this->table);
+    }
+}
